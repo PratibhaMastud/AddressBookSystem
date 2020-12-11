164a2f2fa2558433b4565b6c9eb683b773d6d3ff
PratibhaMastud <karandepratibha999@gmail.com>
2020-12-11 19:40:19 +0530

    [Pratibha Mastud] Add: Edit Contact in address Book

--- AddressBook/AddressBook/AddressBookService.cs
+++ AddressBook/AddressBook/AddressBookService.cs
@@ -7,7 +7,6 @@ namespace AddressBook
     class AddressBookService
     {
 		Person person = new Person();
-
 		// TO ADD NEW CONTACT
 		public void addContact()
 		{
@@ -26,6 +25,28 @@ namespace AddressBook
 			Console.WriteLine("Enter Email Id : ");
 			person.setEmail(Console.ReadLine());
 
+			Console.WriteLine(person);
+		}
+		public void editContact(String firstName, String lastName)
+		{
+			if (firstName.Equals(person.getFirstName()) && lastName.Equals(person.getLastName()))
+			{
+				Console.WriteLine("Enter address : ");
+				person.setAddress(Console.ReadLine());
+				Console.WriteLine("Enter City  : ");
+				person.setCity(Console.ReadLine());
+				Console.WriteLine("Enter state  : ");
+				person.setState(Console.ReadLine());
+				Console.WriteLine("Enter Mobile Number  : ");
+				person.setPhoneNumber(Console.ReadLine());
+				Console.WriteLine("Enter Email Id : ");
+				person.setEmail(Console.ReadLine());
+				Console.WriteLine("Contact updated successfully\n");
+			}
+			else
+			{
+				Console.WriteLine("name not found");
+			}
 			Console.WriteLine(person);
 		}
 	}
--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -6,8 +6,14 @@ namespace AddressBook
     {
         static void Main(string[] args)
         {
+            AddressBookService addressBookServices = new AddressBookService();
             Console.WriteLine("            Welcome to Address Book          ");
             Console.WriteLine("---------------------------------------------");
+            addressBookServices.addContact();
+            Console.WriteLine("Enter first name and last name of the person to edit the contact: ");
+            String firstName = Console.ReadLine();
+            String lastName = Console.ReadLine();
+            addressBookServices.editContact(firstName, lastName);
         }
     }
 }
